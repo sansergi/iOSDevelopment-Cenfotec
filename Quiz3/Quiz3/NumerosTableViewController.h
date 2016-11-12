82ff3e4ba5acdac0299f28868edf628ed3d2e288
sansergi <sergiossv@gmail.com>
2016-11-12 09:14:27 -0600

    Add files via upload

---
+++ Quiz3/Quiz3/NumerosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NumerosTableViewController.h
+//  Quiz3
+//
+//  Created by Estudiantes on 11/12/16.
+//  Copyright © 2016 gv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NumerosTableViewController : UITableViewController
+
+@end
---
+++ Quiz3/Quiz3/NumerosTableViewController.m
@@ -0,0 +1,111 @@
+//
+//  NumerosTableViewController.m
+//  Quiz3
+//
+//  Created by Estudiantes on 11/12/16.
+//  Copyright © 2016 gv. All rights reserved.
+//
+
+#import "NumerosTableViewController.h"
+
+@interface NumerosTableViewController ()
+@property(nonatomic,strong) NSMutableArray *myIntegers;
+@end
+
+@implementation NumerosTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   [self createArray];
+ 
+}
+
+-(void)createArray{
+    
+   
+    
+    for (NSInteger contador = 0; contador < 1000; contador++){
+        
+        [self.myIntegers addObject:[NSNumber numberWithInteger:contador]];
+    
+    };
+    
+  
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+/*- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return self.myIntegers.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+   NSMutableArray *test = self.myIntegers[indexPath.row];
+    
+   
+   cell.textLabel.text = ([[test objectAtIndex:56]stringValue]);
+
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
